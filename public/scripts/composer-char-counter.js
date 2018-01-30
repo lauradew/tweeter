0bc6bdfdbaaa3c3989b971b6d073628648a24631
Laura De Witt <lauramdewitt@gmail.com>
2018-01-30 06:47:29 +0000

    switched handler to refer to self using this

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
   $(".textarea").keyup(function() {
-    $(".counter").text(140 - $(".textarea").val().length);
+    $(".counter").text(140 - $(this).val().length);
   })
 });
