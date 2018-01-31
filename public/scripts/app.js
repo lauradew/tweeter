53a1b501db5c322a024e4991351ff79fea745aae
Laura De Witt <lauramdewitt@gmail.com>
2018-01-31 23:19:14 +0000

    can add new tweets and appear on homepage

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,110 +5,63 @@
  */
 
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
-  }
-];
+//same functionality as easytweet with more tedious formatting
+//more secure but still missing aria-hidden
+// function createTweetElement(tweetObject) {
+
+//   const header = $("<header>");
+//   const article = $("<article>");
+//   const footer = $("<footer>");
+
+//   const avatar = $("<img>").addClass("userAv");
+//   const name = $("<p>").addClass("fullName");
+//   const tag = $("<p>").addClass("userat");
+
+//   const cont = $("<p>").addClass("content");
+
+//   const date = $("<p>").addClass("footerText");
+
+//   const icon = $("<p>").addClass("icons");
+//   const flag = $("<i>").addClass("fa fa-flag");
+//   const retweer = $("<i>").addClass("fa fa-retweet");
+//   const heart = $("<i>").addClass("fa fa-heart");
+
+//   const daysAgo = moment(tweetObject.created_at).fromNow();
 
 
-function createTweetElement(tweetObject) {
-
-  const header = $("<header>");
-  const article = $("<article>");
-  const footer = $("<footer>");
-
-  const avatar = $("<img>").addClass("userAv");
-  const name = $("<p>").addClass("fullName");
-  const tag = $("<p>").addClass("userat");
-
-  const cont = $("<p>").addClass("content");
-
-  const date = $("<p>").addClass("footerText");
-
-  const icon = $("<p>").addClass("icons");
-  const flag = $("<i>").addClass("fa fa-flag");
-  const retweer = $("<i>").addClass("fa fa-retweet");
-  const heart = $("<i>").addClass("fa fa-heart");
-
-  const daysAgo = moment(tweetObject.created_at).fromNow();
+// article.append(header);
+// avatar.attr('src', tweetObject.user.avatars.small);
+// header.append(avatar);
+// name.text(tweetObject.user.name);
+// header.append(name);
+// tag.text(tweetObject.user.handle);
+// header.append(tag);
 
 
-article.append(header);
-avatar.attr('src', tweetObject.user.avatars.small);
-header.append(avatar);
-name.text(tweetObject.user.name);
-header.append(name);
-tag.text(tweetObject.user.handle);
-header.append(tag);
+// cont.text(tweetObject.content.text);
+// article.append(cont);
 
-
-cont.text(tweetObject.content.text);
-article.append(cont);
-
-article.append(footer);
-date.text("Posted " + daysAgo + ".");
-footer.append(date);
-footer.append(icon);
-icon.append(flag);
-icon.append(retweer);
-icon.append(heart);
+// article.append(footer);
+// date.text("Posted " + daysAgo + ".");
+// footer.append(date);
+// footer.append(icon);
+// icon.append(flag);
+// icon.append(retweer);
+// icon.append(heart);
 
 
 
-  console.log(article.html());
-  return article;
-}
+//   console.log(article.html());
+//   return article;
+// }
+//END OF CREATE TWEET ELEMENT FUNCTION
 
 
-// console.log($tweet);
 $(document).ready(function() {
   // var $tweet = createTweetElement(tweetData);
   // $('#tweets-container').append($tweet);
-  // console.log(data);
-  renderTweets(data);
-});
+  loadTweets();
+
 
 function renderTweets(tweets) {
   tweets.forEach(function(tweet) {
@@ -116,6 +69,66 @@ function renderTweets(tweets) {
     $('#tweets-container').append(easyTweet(tweet));
   });
 }
+
+$('.new-tweet form').on('submit', function(e) {
+  e.preventDefault();
+  const newtext = $(this).find(".textarea").val();
+  if (newtext.length === 0 ||
+    newtext.length > 140) {
+    alert("Tweet must contain characters, and be less than 140 characters.");
+  return;
+  } else {
+  var newest = $('.new-tweet form').serialize();
+
+  $.post('/tweets', newest).done(function() {
+    loadTweets();
+
+    // load that tweet into the view
+    // $('#tweets-container').append(easyTweet(newest));
+
+  });
+}
+});
+
+function loadTweets() {
+    $.ajax({
+      url: '/tweets',
+      method: 'GET',
+      success: function(result) {
+        renderTweets(result);
+      }
+    })
+}
+
+
+// function addTweet() {
+//   $.ajax({
+//     url: '/tweets',
+//     method: 'POST',
+//     success: function(result) {
+
+//       if ($('.new-tweet form') === null ||
+//         $('.new-tweet form').length > 140) {
+//         alert("Tweet must be less than 140 characters.");
+//       return;
+//       } else {
+
+
+//       easyTweet(result);
+//       }
+//     }
+//   })
+// }
+
+//same function as loadTweets;
+// $(() => {
+// function loadTweets() {
+//   $.get('/tweets').done(function(result) {
+//     renderTweets(result);
+//   })
+
+
+
 
 
 function easyTweet(tweetObject) {
@@ -150,5 +163,6 @@ function easyTweet(tweetObject) {
 
 
 
+});
 
 
