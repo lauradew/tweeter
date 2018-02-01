025ab2dc670497f9f5a551e189d9e080aa95270a
Laura De Witt <lauramdewitt@gmail.com>
2018-02-01 22:18:46 +0000

    switched to mongodb storage

--- mongo_example.js
+++ mongo_example.js
@@ -1,6 +1,8 @@
 "use strict";
 
-const MongoClient = require("mongodb").MongoClient;
+//commented out is does same as line below
+// const MongoClient = require("mongodb").MongoClient;
+const {MongoClient} = require("mongodb");
 const MONGODB_URI = "mongodb://localhost:27017/tweeter";
 
 MongoClient.connect(MONGODB_URI, (err, db) => {
@@ -9,16 +11,31 @@ MongoClient.connect(MONGODB_URI, (err, db) => {
     throw err;
   }
 
-  // ==> We have a connection to the "test-tweets" db,
-  //     starting here.
+  // We have a connection to the "tweeter" db, starting here.
   console.log(`Connected to mongodb: ${MONGODB_URI}`);
 
-  // ==> Let's "get all tweets". In Mongo-speak, we "find" them.
-  db.collection("tweets").find().toArray((err, results) => {
+  // ==> Refactored and wrapped as new, tweet-specific function:
+
+  function getTweets(callback) {
+    db.collection("tweets").find().toArray((err, tweets) => {
+      if (err) {
+        return callback(err);
+      }
+      callback(null, tweets);
+    });
+  }
+
+  // ==> Later it can be invoked. Remember even if you pass
+  //     `getTweets` to another scope, it still has closure over
+  //     `db`, so it will still work. Yay!
+
+  getTweets((err, tweets) => {
     if (err) throw err;
 
-    console.log("results array: ", results);
-
+    console.log("Logging each tweet:");
+    for (let tweet of tweets) {
+      console.log(tweet);
+    }
 
     db.close();
   });
