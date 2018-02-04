7e613fe7a7e333752e3cc689df5c490eeb47d6dc
Laura De Witt <lauramdewitt@gmail.com>
2018-02-04 04:05:19 +0000

    Fix mongo connection string issue, initialized scss folders

--- server/index.js
+++ server/index.js
@@ -2,17 +2,29 @@
 
 // Basic express setup:
 
+require('dotenv').config();
 const PORT          = 8080;
 const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
+
+const nodeSass      = require("node-sass-middleware");
+const path          = require("path");
+
+// app.use(nodeSass({
+//   src: path.join(__dirname, 'sass'),
+//   dest: path.join(__dirname, 'public'),
+//   debug: true;
+//   outputStyle: 'compressed'
+// }));
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // The in-memory database of tweets. It's a basic object with an array in it.
 const MongoClient = require("mongodb").MongoClient;
-const MONGODB_URI = "mongodb://localhost:27017/tweeter";
+const MONGODB_URI = process.env.MONGODB_URI;
 // const db = MongoClient;
 
 MongoClient.connect(MONGODB_URI, (err, db) => {
